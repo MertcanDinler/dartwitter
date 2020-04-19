98d404a09ab36b4783c57ebefc1834c8afb25efd
Mertcan Dinler <mertdinlerin@gmail.com>
2020-04-19 18:28:05 +0300

    oauth1_handler minor changes

--- lib/src/auth/oauth1_handler.dart
+++ lib/src/auth/oauth1_handler.dart
@@ -6,10 +6,11 @@
 // https://raw.githubusercontent.com/mrtcndnlr/dartwitter/master/LICENSE
 //
 // Created:  2020-04-18T21:59:38.051Z
-// Modified: 2020-04-18T22:05:39.349Z
+// Modified: 2020-04-19T15:26:05.257Z
 //
 
-import 'package:oauth1_client/oauth1_client.dart' as oauth1_client;
+import 'package:oauth1_client/oauth1_client.dart' as auth;
+import 'package:http/http.dart' as http;
 
 import '../models/access_token.dart';
 import 'auth_base.dart';
@@ -17,20 +18,29 @@ import 'auth_base.dart';
 /// OAuth1 Authentication Handler
 class OAuth1Handler extends AuthBase {
   final String _oAuthUrl = 'https://api.twitter.com/oauth/';
-  oauth1_client.OAuth1Client _oauth1;
+  auth.OAuth1Client _oauth1;
+  String _authorizedUsername;
+  http.BaseClient _client;
 
   OAuth1Handler(String apiKey, String apiKeySecret, {callbackUrl = 'oob'}) {
-    var credentials = oauth1_client.ConsumerCredentials(apiKey, apiKeySecret,
+    var credentials = auth.ConsumerCredentials(apiKey, apiKeySecret,
         callbackUrl: callbackUrl);
-    _oauth1 = oauth1_client.OAuth1Client(credentials);
-    _oauth1.signatureMethod = oauth1_client.Signatures.hmacSha1;
+    _oauth1 = auth.OAuth1Client(credentials);
+    _oauth1.signatureMethod = auth.Signatures.hmacSha1;
+    _client = _oauth1.client;
   }
+
+  String get authorizedUsername => _authorizedUsername;
 
   /// After user has authorized the request token, get access token
   /// with user supplied verifier.
   Future<AccessToken> getAccessToken(String verifier) async {
     var accessToken =
         await _oauth1.fetchAccessToken(_getOAuthUrl('access_token'), verifier);
+    var splitResp = Uri.splitQueryString(_oauth1.lastResponse.body);
+    if (splitResp.containsKey('screen_name')) {
+      _authorizedUsername = splitResp['screen_name'];
+    }
     return AccessToken(accessToken.identifier, accessToken.secret);
   }
 
