8e8b9e28dc1895ed927c726c4b83043ece1472d1
Mertcan Dinler <mertdinlerin@gmail.com>
2020-04-26 15:02:09 +0300

    renamed methods

--- lib/src/endpoints/users.dart
+++ lib/src/endpoints/users.dart
@@ -6,7 +6,7 @@
 // https://raw.githubusercontent.com/mrtcndnlr/dartwitter/master/LICENSE
 //
 // Created:  2020-04-24T22:51:04.462Z
-// Modified: 2020-04-25T22:39:45.535Z
+// Modified: 2020-04-26T12:01:53.767Z
 //
 
 import 'dart:convert';
@@ -166,7 +166,7 @@ mixin Users on ApiBase {
   /// to allow paging back and forth.
   /// [count] The number of users to return per page, up to a maximum of 200.
   /// Defaults to 20.
-  Future<CursorResponse<User>> friendsList(
+  Future<CursorResponse<User>> friends(
       {int id,
       String screenName,
       int cursor,
@@ -201,7 +201,7 @@ mixin Users on ApiBase {
   /// to allow paging back and forth.
   /// [count] The number of users to return per page, up to a maximum of 200.
   /// Defaults to 20.
-  Future<CursorResponse<User>> followersList(
+  Future<CursorResponse<User>> followers(
       {int id,
       String screenName,
       int cursor,
@@ -223,5 +223,35 @@ mixin Users on ApiBase {
     };
     var resp = await request(method, endPoint, parameters: parameters);
     return CursorResponse.fromJson<User>(resp);
+  }
+
+  /// Returns a collection of integer IDs for every user who has a pending
+  /// request to follow the authenticating user.
+  /// [cursor] Causes the list of connections to be broken into pages of no more
+  /// than 5000 IDs at a time. The number of IDs returned is not guaranteed to
+  /// be 5000 as suspended users are filtered out after connections are queried.
+  /// If no cursor is provided, a value of -1 will be assumed, which is the f
+  /// irst "page."
+  Future<CursorResponse<int>> friendshipsIncoming({int cursor}) async {
+    var method = 'GET';
+    var endPoint = 'friendships/incoming';
+    var parameters = {'cursor': cursor?.toString()};
+    var resp = await request(method, endPoint, parameters: parameters);
+    return CursorResponse.fromJson<int>(resp);
+  }
+
+  /// Returns a collection of integer IDs for every user who has a pending
+  /// request to follow the authenticating user.
+  /// [cursor] Causes the list of connections to be broken into pages of no more
+  /// than 5000 IDs at a time. The number of IDs returned is not guaranteed to
+  /// be 5000 as suspended users are filtered out after connections are queried.
+  /// If no cursor is provided, a value of -1 will be assumed, which is the f
+  /// irst "page."
+  Future<CursorResponse<int>> friendshipsOutgoing({int cursor}) async {
+    var method = 'GET';
+    var endPoint = 'friendships/outgoing';
+    var parameters = {'cursor': cursor?.toString()};
+    var resp = await request(method, endPoint, parameters: parameters);
+    return CursorResponse.fromJson<int>(resp);
   }
 }
