45592b851ae89b6ffb1ea697677efe7b04ba1336
Mertcan Dinler <mertdinlerin@gmail.com>
2020-04-27 01:14:59 +0300

    added create & destroy friendship methods

--- lib/src/endpoints/fiendships.dart
+++ lib/src/endpoints/fiendships.dart
@@ -6,8 +6,10 @@
 // https://raw.githubusercontent.com/mrtcndnlr/dartwitter/master/LICENSE
 //
 // Created:  2020-04-26T13:43:34.870Z
-// Modified: 2020-04-26T13:45:37.768Z
+// Modified: 2020-04-26T22:13:33.125Z
 //
+
+import 'package:dartwitter/models.dart';
 
 import '../api_base.dart';
 import '../models/cursor_response.dart';
@@ -41,5 +43,35 @@ mixin Friendships on ApiBase {
     var parameters = {'cursor': cursor?.toString()};
     var resp = await request(method, endPoint, parameters: parameters);
     return CursorResponse.fromJson<int>(resp);
+  }
+
+  Future<User> createFriendship(
+      {int id, String screenName, bool follow}) async {
+    if (id == null && screenName == null) {
+      throw ArgumentError('required ids or screenNames parameters.');
+    }
+    var method = 'POST';
+    var endPoint = 'friendships/create';
+    var parameters = {
+      'user_id': id?.toString(),
+      'screen_name': screenName?.toString(),
+      'follow': follow?.toString()
+    };
+    var resp = await request(method, endPoint, parameters: parameters);
+    return User.fromJson(resp);
+  }
+
+  Future<User> destroyFriendship({int id, String screenName}) async {
+    if (id == null && screenName == null) {
+      throw ArgumentError('required ids or screenNames parameters.');
+    }
+    var method = 'POST';
+    var endPoint = 'friendships/destroy';
+    var parameters = {
+      'user_id': id?.toString(),
+      'screen_name': screenName?.toString()
+    };
+    var resp = await request(method, endPoint, parameters: parameters);
+    return User.fromJson(resp);
   }
 }
